dd0006d9d1fecdd1e8b8a57d277b7ce4185fd634
Andrew Burgess <andrew@deceptacle.com>
2012-05-24 14:52:41 -0400

    Loads upcoming tracks better. Caches some stuff

--- js/application.js
+++ js/application.js
@@ -8,7 +8,6 @@ var playlist = new models.Playlist();
 var lastFM = new LastFM();
 var upcoming = new Array();
 
-var resizeLock = false;
 var completedLock = false;
 
 $(function() {
@@ -17,18 +16,10 @@ $(function() {
     
     updateCurrentInfo();
     updateUpcomingTracks();
-    
-    $(window).resize(function () {
-        if (!resizeLock)
-            setTimeout(resizeWindow, 500);
-            
-        resizeLock = true;
-    });
 });
 
 function handlePlayerChanged(event) {    
     if (event.data.curtrack) {
-        var runningLock = false;
         updateCurrentInfo();
         updateUpcomingTracks();
     }
@@ -51,30 +42,38 @@ function updateCurrentInfo() {
     });
     
     var imgCount = Math.floor(Math.min($('.bio').width() / 100.0, 25));
+    if ($('.bio').width() % 100.0 > 0) imgCount++;
     getImages(artist, imgCount);
 }
 
 function updateUpcomingTracks() {
+    for (var i = 1; i <= 3; i++) {
+        $('.upcoming').find('#col' + i).empty().append($('<div />').addClass('throbber').append($('<div />')));
+    }
+
     var currentArtist = player.track.data.artists[0].name;
     upcoming = new Array();
     completedLock = false;
     lastFM.makeRequest('artist.getSimilar', {artist: currentArtist, limit: 15, autocorrect: 1}, function (data) {
+        setTimeout(function() {
+            if (upcoming.length != 3) updateUpcomingTracks();
+        }, 5000);
+    
         var artistIndexes = new Array();
         for (var i = 0; i < 15; i++) artistIndexes[i] = i;
         artistIndexes.shuffle();
         
-        for (var i = 0; i < artistIndexes.length; i++) {
+        for (var i = 0; i < 5; i++) {
             var artist = data.similarartists.artist[artistIndexes[i]];
             var a = artist.name;
+            console.log(a);
             
             if (upcoming.length == 3) {
-                processUpcoming();
                 return;
             }
             
             lastFM.makeRequest('artist.getTopTracks', {artist: a, autocorrect: 1, limit: 10}, function (toptracks) {
                 if (upcoming.length == 3) {
-                    processUpcoming();
                     return;
                 }
                 
@@ -83,10 +82,9 @@ function updateUpcomingTracks() {
                 for (var i = 0; i < 10; i++) trackIndexes[i] = i;
                 trackIndexes.shuffle();
                 
-                for (var i = 0; i < trackIndexes.length; i++) {
+                for (var i = 0; i < 3; i++) {
                     var track = toptracks.toptracks.track[trackIndexes[i]];
                     if (upcoming.length == 3) {
-                        processUpcoming();
                         return;
                     }
                     var query = 'artist:' + a + ' track:' + track.name;
@@ -97,19 +95,17 @@ function updateUpcomingTracks() {
                     search.searchArtists = false;
                     search.searchPlaylists = false;
                     search.observe(models.EVENT.CHANGE, function () {
-                        if (upcoming.length == 3) {
-                            processUpcoming();
+                        if (upcoming.length == 3 || found) {
                             return;
                         }
                         
-                        if (found) return;
-                        
                         if (search.tracks.length > 0) {
+                            console.log('pushing ' + search.tracks[0].data.uri);
+                            console.log(upcoming.length);
                             upcoming.push(search.tracks[0].data.uri);
+                            console.log(upcoming.length);
+                            processUpcoming(search.tracks[0].data.uri, upcoming.length);
                             found = true;
-                            
-                            if (upcoming.length == 3)
-                                processUpcoming();
                         }
                     });
                     
@@ -120,38 +116,19 @@ function updateUpcomingTracks() {
     });
 }
 
-function processUpcoming() {
-    if (completedLock) return;
-    completedLock = true;
-    
-    $.each(upcoming, function (index, entry) {
-        var el = $('.upcoming').find('#col' + (index + 1)).empty();
-        models.Track.fromURI(entry, function (track) {
-            var img = new views.Image(track.data.album.cover);
-            el.prepend($('<div />').addClass('artist-image').
-                                    css('height', el.width()).
-                                    css('width', el.width()).append(
-                                        $(img.node).css('height', el.width()).css('width', el.width())
-                                    ));
-            
-            el.append($('<h1 />').text(track.artists[0].name.decodeForText())).append($('<h2 />').text(track.name.decodeForText()));
-        });
+function processUpcoming(uri, index) {
+    console.log("processing " + index);
+    var el = $('.upcoming').find('#col' + index).empty();
+    models.Track.fromURI(uri, function (track) {
+        var img = new views.Image(track.data.album.cover);
+        el.prepend($('<div />').addClass('artist-image').
+                                css('height', el.width()).
+                                css('width', el.width()).append(
+                                    $(img.node).css('height', el.width()).css('width', el.width())
+                                ));
+        
+        el.append($('<h1 />').text(track.artists[0].name.decodeForText())).append($('<h2 />').text(track.name.decodeForText()));
     });
-}
-
-function resizeWindow() {
-    var imgCount = Math.floor(Math.min($('.bio').width() / 100.0, 25));
-    if ($('#artist-images').find('img').length > imgCount) {
-        $('#artist-images').find('img:gt(' + (imgCount - 1) + ')').remove();
-    } else if ($('#artist-images').find('img').length == imgCount) {
-        //Do nothing
-    } else {
-        $('#artist-images').empty();
-        var artist = player.track.data.artists[0].name;
-        getImages(artist, imgCount);
-    }
-    
-    resizeLock = false;
 }
 
 function getImages(artist, limit) {
--- js/lastfm.js
+++ js/lastfm.js
@@ -1,4 +1,5 @@
 (function ($, window, undefined) {
+    var cleanupCacheLock = false;
     
     function LastFM() {
         this.key = "21edbb30193dc36e6fb21cc57b1d8e18";
@@ -6,6 +7,10 @@
 
     LastFM.prototype.makeRequest = function (method, args, callback) {
         var self = this;
+        
+        if (!cleanupCacheLock)
+            setTimeout(function() { self.cleanupCache(); }, 500);
+        cleanupCacheLock = true;
         
         args.api_key = self.key;
         args.format = "json";
@@ -26,6 +31,7 @@
             url: "http://ws.audioscrobbler.com/2.0/",
             success: function (data) {
                 console.log("LASTFM: Received data", data);
+                data.accessed = new Date().getTime();
                 if (self.checkResponse(data)) {
                     localStorage.setItem(key, JSON.stringify(data));
                     callback(data);
@@ -49,6 +55,41 @@
             return true;
         }
     };
+    
+    LastFM.prototype.cleanupCache = function (lastAccess, count) {
+        if (count && count <= 0) {
+            cleanupCacheLock = false;
+            return;
+        }
+        var remaining = 1024 * 1024 * 5 - unescape(encodeURIComponent(JSON.stringify(localStorage))).length;
+        console.log('Remaining space in cache: ' + Math.round((remaining / 1024.0), 2) + ' KB');
+        if (remaining < 100000) {
+            console.log('Cleaning cache');
+            
+            var toRemove = new Array();
+            if (lastAccess === undefined) lastAccess = new Date().getTime() - 3600000;
+            for (var i = 0; i < localStorage.length; i++) {
+                var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                if (obj.accessed < lastAccess) {
+                    toRemove.push(localStorage.key(i));
+                }
+            }
+            
+            if (toRemove.length == 0) {
+                if (count === undefined) count = 3;
+                count = count - 1;
+                return this.cleanupCache(lastAccess / 2, count--);
+            }
+            
+            console.log('Removing ' + toRemove.length + ' items');
+            for (var i = 0; i < toRemove.length; i++) {
+                console.log("Removing " + toRemove[i]);
+                localStorage.removeItem(toRemove[i]);
+            }
+        }
+        
+        cleanupCacheLock = false;
+    }
     
     window.LastFM = LastFM;
 
